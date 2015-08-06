4eb95bde1807ee912cc90f6d19578f81df618094
VincenzoDocker <vincyfetta91@libero.it>
2015-08-06 15:59:57 +0200

    testOnExistShortUrl

---
+++ URLShortener/src/main/java/URLShortener/Test/RedisConnection_Test.java
@@ -0,0 +1,61 @@
+package URLShortener.DAO;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import URLShortener.Utility.UndesiderableWords;
+
+public class RedisConnection_Test extends TestCase{
+
+	String [] a  ={"www.google.it","www.amazon.com","www.vincenzoMOlfettaInf.it"};
+	boolean [] b  ={false,true,true};
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+//
+//	@Test
+//	public void testSetKeyData() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetValue() {
+//		fail("Not yet implemented");
+//	}
+
+	@Test
+	public void testIsExists() {
+		
+		for (int i = 0; i < a.length; i++) {
+			boolean c = RedisConnection.isExists(a[i]);
+
+			assertTrue(c == b[i]);
+		}
+	}
+
+//	@Test
+//	public void testGetValue() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testUpdate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetKeyData() {
+//		fail("Not yet implemented");
+//	}
+
+}

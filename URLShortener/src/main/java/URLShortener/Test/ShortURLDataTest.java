73ec58d8919a00061057d3c2c9929390937cea7a
VincenzoDocker <vincyfetta91@libero.it>
2015-09-07 18:07:45 +0200

    Update result conflict

--- URLShortener/src/main/java/URLShortener/Test/ShortURLDataTest.java
+++ URLShortener/src/main/java/URLShortener/Test/ShortURLDataTest.java
@@ -14,18 +14,10 @@ import static org.junit.Assert.*;
  */
 public class ShortURLDataTest extends TestCase {
 
-<<<<<<< HEAD
     int i=0;
     int j=0;
     String[] casoPrimo = {"www.sht.com/kWpcv","www.sht.com/4nlzR","www.sht.com/WIPmq","www.sht.com/5fDj0","www.sht.com/1RnpB"};
     String[] casoSecondo = {"www.caccapupu.com","www.gooogle.it!","www.amazon.sl","www.uniba.com","www.shazam.sc"};
-=======
-
-    String[] casoPrimo = {"broJG","SQhAJ","GHoHp","q8xTB","vlynY","IRosu","NuUr8","cxmDH"};
-    String[] casoSecondo = {"www.google.it","www.amazon.com","www.basididatiavanzati.de","https://www.focus.it","www.ciaosic.sl","www.happycasa.org","www.ultimoesameesto.ciao","www.tempesta.civediamo"};
->>>>>>> origin/master
-
-
 
     /**
      * @throws Exception
@@ -56,24 +48,21 @@ public class ShortURLDataTest extends TestCase {
     /**
      * @throws Exception
      */
-<<<<<<< HEAD
     @Test
     // per funzionare questo test ovviamente i casi da testare devono essere inclusi all'interno del database redis
     //se non lo sono prima generate uno short url dal long
     // dopo di che testate che il metodo sia corretto
-=======
-    /*@Test
->>>>>>> origin/master
+
     public void testSaveShortLongURL() throws Exception {
         for(i=0;i<casoPrimo.length;i++){
             for(j=0;j<casoSecondo.length;j++){
 
-                boolean result = test.saveShortLongURL(casoPrimo[i], casoSecondo[j]);
+                boolean result = ShortURLData.saveShortLongURL(casoPrimo[i], casoSecondo[j]);
                 assertTrue(result==false);
             }
         }
 
-    }*/
+    }
 
 
 }
